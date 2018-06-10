0cce11e45938dd78356753126caeb441551cf623
Peter Gundel <mail@petergundel.de>
2018-06-10 16:08:16 +0200

    ✨ Add first test case

--- src/24Solver.spec.ts
+++ src/24Solver.spec.ts
@@ -1,7 +1,29 @@
-import { solve } from './24Solver'
+import { InputNumber, solve } from './24Solver'
 
 describe('solve', () => {
-  it('responds', () => {
-    expect(solve()).toBe(true)
+  type TestCaseInput = [InputNumber, InputNumber, InputNumber, InputNumber]
+  type TestCase = [TestCaseInput, string[]]
+  const testCases: TestCase[] = [
+    [
+      [1, 1, 2, 7],
+      [
+        '(1 + 2) * (1 + 7)',
+        '(1 + 2) * (7 + 1)',
+        '(1 + 7) * (1 + 2)',
+        '(1 + 7) * (2 + 1)',
+        '(2 + 1) * (1 + 7)',
+        '(2 + 1) * (7 + 1)',
+        '(7 + 1) * (1 + 2)',
+        '(7 + 1) * (2 + 1)',
+      ],
+    ],
+  ]
+  testCases.map(testCase => {
+    const [input, output] = testCase
+    it(`matches the output for ${input}`, () => {
+      // Array spread for functions does not work in Typescript yet
+      // https://github.com/Microsoft/TypeScript/issues/4130
+      expect(solve(input[0], input[1], input[2], input[3])).toEqual(output)
+    })
   })
 })
--- src/24Solver.ts
+++ src/24Solver.ts
@@ -1,3 +1,19 @@
-export function solve() {
-  return true
+export type InputNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+export function solve(
+  first: InputNumber,
+  second: InputNumber,
+  third: InputNumber,
+  forth: InputNumber,
+): string[] {
+  return [
+    '(1 + 2) * (1 + 7)',
+    '(1 + 2) * (7 + 1)',
+    '(1 + 7) * (1 + 2)',
+    '(1 + 7) * (2 + 1)',
+    '(2 + 1) * (1 + 7)',
+    '(2 + 1) * (7 + 1)',
+    '(7 + 1) * (1 + 2)',
+    '(7 + 1) * (2 + 1)',
+  ]
 }
